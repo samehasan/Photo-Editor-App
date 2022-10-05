c907b559bda5c76f39be266f7d1d109bbf40690d
samehasan <samehasan25@gmail.com>
2022-10-06 00:26:14 +0600

    upd

---
+++ app/src/main/java/com/PhotoEditor/S2/MainActivity.java
@@ -0,0 +1,50 @@
+package com.PhotoEditor.S2;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.PhotoEditor.S2.databinding.ActivityMainBinding;
+
+import java.io.ByteArrayOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    int IMAGE_REQUEST_CODE=45;
+   @Override
+           protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       binding=ActivityMainBinding.inflate(getLayoutInflater());
+       setContentView(binding.getRoot());
+
+       binding.editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, IMAGE_REQUEST_CODE);
+            }
+        });
+
+
+
+
+        }
+    }
